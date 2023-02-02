2d43c85b5770b7ffe77948cc9d53e0655609469c
really650a <99811385+really650a@users.noreply.github.com>
2023-02-02 21:06:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,13 @@ from pyrogram import Client, filters, idle
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyromod import listen
 from pyrogram import enums
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://really651:K4vSnRMEsZhqsTqS@cluster0.pxc2foz.mongodb.net/?retryWrites=true&w=majority")
+
+db = client["Products"]
+collection = db["coll"]
 
 api_id = 12501338
 api_hash = "d2f2d3b3eae3df3dac595d3c8f55d443"
@@ -19,20 +26,23 @@ b = InlineKeyboardMarkup([
 ]
 )
 
-
-
 @app.on_message(filters.command("dev") & filters.private)
 async def dev(cilent, message):
 	await app.send_message(message.from_user.id, "👨‍💻Bot Developer👨‍💻", reply_markup = b)
 
 @app.on_message(filters.command("start") & filters.private)
 async def sft(cilent, message):
-	await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Save Restricted Messages bot. This bot can help you to save restricted content from <b>public channel and Group</b>.\n\n✍️Send /save to save restricted content✨", parse_mode = enums.ParseMode.HTML)
-
+	ids = message.from_user.id
+	users= collection.find_one({"user_id": ids})
+	if users:
+		await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Save Restricted Messages bot. This bot can help you to save restricted content from <b>public channel and Group</b>.\n\n✍️Send /save to save restricted content✨", parse_mode = enums.ParseMode.HTML)
+	else:
+		collection.insert_one({"user_id": ids})
+		await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Save Restricted Messages bot. This bot can help you to save restricted content from <b>public channel and Group</b>.\n\n✍️Send /save to save restricted content✨", parse_mode = enums.ParseMode.HTML)
 
 @app.on_message(filters.command("save") & filters.private)
 async def start(client, message):
-   ask = await app.ask(message.from_user.id, "🌀Enter a link that i'll save it's content.\n🚫To Cancel send /cancel.")
+   ask = await message.chat.ask("🌀Enter a link that i'll save it's content.\n🚫To Cancel send /cancel.")
    if ask.text =="/cancel": 
    	await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Media Saver bot. This bot can help you to save restricted content from <b>public channel and Group</b>.\n\n✍️Send /save to save restricted content✨", parse_mode = enums.ParseMode.HTML)
    	return 
@@ -84,7 +94,6 @@ async def start(client, message):
    		else:
    			await message.reply("⚠️Either i don\'t know this type of conent! or I couldn't save it.")
    	except Exception as e:
-   		print(e)
    		await message.reply("🔰Oppss! Make sure that the channel is public and the link is starts with <b>https://</b>", parse_mode = enums.ParseMode.HTML)
    
 print("Started")   
