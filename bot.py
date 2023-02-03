9690b12ab01ba49f0da19e0f01054fb7278d51b0
really650a <99811385+really650a@users.noreply.github.com>
2023-02-03 08:54:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ api_id = 11855414
 api_hash = "71449899c824b5bc9a91d8a52b20c5f3"
 bot_token = "5624770835:AAEyK318nLW7PlVGWsezEYYNnBgCNJL4P40-Q"
 app = Client(
-  "MediaSaver",
+  "MediaSaver1",
   api_id=api_id,
   api_hash=api_hash,
   bot_token=bot_token
@@ -48,8 +48,8 @@ async def start(client, message):
     user = collection.find_one({'user_id': chat_id})
     if user and 'bonus_time' in user:
         bonus_time = user['bonus_time']
-        if now - bonus_time < datetime.timedelta(minutes=10):
-            await app.send_message(chat_id, '🙂You have to wait 10 minutes in order to send another task!')
+        if now - bonus_time < datetime.timedelta(hours=24):
+            await app.send_message(chat_id, '🙂You have to wait 5 minutes in order to send another task!')
             return
         else:
             collection.update_one({'user_id': chat_id}, {'$set': {'bonus_time': now}})
@@ -58,65 +58,60 @@ async def start(client, message):
     else:
         collection.insert_one({'user_id': chat_id, 'bonus_time': now})
         pass
-        #bot.send_message(chat_id, 'You have successfully received your bonus')
-   ask = await message.chat.ask("🌀Enter a link that i'll save it's content.\n🚫To Cancel send /cancel.")
-   if ask.text =="/cancel": 
-   	await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Media Saver bot. This bot can help you to save restricted content from <b>public channel and Group</b>.\n\n✍️Send /save to save restricted content✨", parse_mode = enums.ParseMode.HTML)
-   	return 
-   else:
-   	pass   	
-   if "/" in message.text:  	
-   	try:
-   		s = str(ask.text[13:])
-   		m = s.split("/")[0]
-   		s1 = f"@{m}" 		
-   		m1 = s.split("/")[1]
-   		a = await app.get_messages(s1, int(m1))   		   			
-   		if a.audio:
-   			d = a.audio.file_id			
-   			f = a.audio.file_name
-   			det = await message.reply("♻️Wait a moment...")
-   			c = await app.download_media(d)
-   			await app.send_audio(message.chat.id, c, file_name = f)
-   			await app.delete_messages(message.from_user.id, det.id)
-   			return 
-   		if a.poll:
-   			await message.reply("🙁For Now I don\'t Save Quiz & Poll!")
-   			return 
-   		if a.text:
-   			await message.reply(a.text)
-   		elif a.video:			
-   			d = a.video.file_id
-   			cap = a.caption
-   			f = a.video.file_name
-   			det = await message.reply("♻️Wait a moment...")
-   			c = await app.download_media(d)
-   			await app.send_video(message.chat.id, c, caption = cap, file_name = f)
-   			await app.delete_messages(message.from_user.id, det.id)
-   		elif a.photo:
-   			d = a.photo.file_id
-   			cap = a.caption
-   			det1 = await message.reply("♻️Wait a moment...")
-   			c = await app.download_media(d)
-   			await app.send_photo(message.chat.id, c, caption = cap)
-   			await app.delete_messages(message.from_user.id, det1.id)
-   		elif a.document:			
-   			d = a.document.file_id
-   			cap = a.caption
-   			f = a.document.file_name
-   			#file = await app.get_file(d)
-#   			print(f"{file.size / 1024 / 1024}")
-   			det2 = await message.reply("♻️Wait a moment...")
-   			c = await app.download_media(d)
-   			await app.send_document(message.chat.id, c, caption = cap, file_name = f)
-   			await app.delete_messages(message.from_user.id, det2.id)
-   		else:
-   			await message.reply("⚠️Either i don\'t know this type of conent! or I couldn't save it.")
-   	except Exception as e:
-   		await message.reply("🔰Oppss! Make sure that the channel is public and the link is starts with <b>https://</b>", parse_mode = enums.ParseMode.HTML)
+    ask = await message.chat.ask("🌀Enter a link that i'll save it's content.\n🚫To Cancel send /cancel.")
+    if ask.text =="/cancel":
+    	await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Media Saver bot. This bot can help you to save restricted content from <b>public channel and Group</b>.\n\n✍️Send /save to save restricted content✨", parse_mode = enums.ParseMode.HTML)
+    	return
+    else:
+    	pass
+    if "/" in message.text:
+    	try:
+    		s = str(ask.text[13:])
+    		m = s.split("/")[0]
+    		s1 = f"@{m}"
+    		m1 = s.split("/")[1]
+    		a = await app.get_messages(s1, int(m1))
+    		if a.audio:
+    			d = a.audio.file_id
+    			f = a.audio.file_name
+    			det = await message.reply("♻️Wait a moment...")
+    			c = await app.download_media(d)
+    			await app.send_audio(message.chat.id, c, file_name = f)
+    			await app.delete_messages(message.from_user.id, det.id)
+    			return
+    		if a.poll:
+    			await message.reply("🙁For Now I don\'t Save Quiz & Poll!")
+    			return 
+    		if a.text:
+    			await message.reply(a.text)
+    		elif a.video:
+    			d = a.video.file_id
+    			cap = a.caption
+    			f = a.video.file_name
+    			det = await message.reply("♻️Wait a moment...")
+    			c = await app.download_media(d)
+    			await app.send_video(message.chat.id, c, caption = cap, file_name = f)
+    			await app.delete_messages(message.from_user.id, det.id)
+    		elif a.photo:
+    			d = a.photo.file_id
+    			cap = a.caption
+    			det1 = await message.reply("♻️Wait a moment...")
+    			c = await app.download_media(d)
+    			await app.send_photo(message.chat.id, c, caption = cap)
+    			await app.delete_messages(message.from_user.id, det1.id)
+    		elif a.document:
+    			d = a.document.file_id
+    			cap = a.caption
+    			f = a.document.file_name
+    			det2 = await message.reply("♻️Wait a moment...")
+    			c = await app.download_media(d)
+    			await app.send_document(message.chat.id, c, caption = cap, file_name = f)
+    			await app.delete_messages(message.from_user.id, det2.id)
+    		else:
+    			await message.reply("⚠️Either i don\'t know this type of content! or I couldn't save it.")
+    	except Exception as e:
+    		await message.reply("🔰Oppss! Make sure that the channel is public and the link is starts with <b>https://</b>", parse_mode = enums.ParseMode.HTML)
 
       
 print("Started")   
 app.run()   
-
-
