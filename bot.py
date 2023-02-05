c12faa62a278859e399237c854c3531ebaa73717
really650a <99811385+really650a@users.noreply.github.com>
2023-02-05 08:55:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,9 +21,43 @@ api_hash = "71449899c824b5bc9a91d8a52b20c5f3",
 bot_token = "5624770835:AAEyK318nLW7PlVGWsezEYYNnBgCNJL4P40"
 )
 
+help = """<u>    ⚪HELP SECTION</u>
+
+<b>👌As mentioned already this bot is used to get restricted content from Public Telegram Channel! You don\'t have to worry about file size this bot can send upto 4GB file🤩 And also its fastest than any other bots. its better if you see yourself😎
+
+🔍To Get Restricted Content, Just send the link of the message🔍
+
+🤨However there is limit while sending video and documents. but thats not bad and there is no limit with other contents✨
+
+🛠 If you get bug/issues report us @Developerschat 👥</b>
+"""
+
+key = InlineKeyboardMarkup([
+[InlineKeyboardButton(text ="🌀Project Channel", url="t.me/DevelopersPage"), InlineKeyboardButton(text ="⚙️Support Group", url = "t.me/developerschat")],
+[InlineKeyboardButton(text ="🤔 Help 🗞", callback_data ="help"), InlineKeyboardButton(text ="✍️ Developer 🔍", user_id = 1365625365)]
+])
+
+back = InlineKeyboardMarkup([
+[InlineKeyboardButton(text ="🔙Back", callback_data ="back")]
+])
+
+@app.on_callback_query()
+async def ans(client, callback):
+	if callback.data =="help":
+		await app.edit_message_text(callback.message.chat.id, callback.message.id, help, reply_markup = back, parse_mode = enums.ParseMode.HTML)
+	if callback.data =="back":
+		await app.edit_message_text(callback.message.chat.id, callback.message.id, f"👋Hello {callback.from_user.mention} \n\nWelcome Save Restricted Messages bot. This bot can help you to save restricted content from <b>public channel! Even upto 4GB file!</b>\n\n✍️Just Send me the link of the message🤩", parse_mode = enums.ParseMode.HTML, reply_markup = key)
+
+async def checking(client, message):
+	try:
+		a = await app.get_chat_member("@channel", message.from_user.id).status 
+		return True 
+	except:
+		return False
+
 @app.on_message(filters.command("start"))
 async def strf(client, message):
-		await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Save Restricted Messages bot. This bot can help you to save restricted content from <b>public channel! Even upto 4GB file!</b>.\n\n✍️Just Send me the link of Message✨", parse_mode = enums.ParseMode.HTML)
+		await message.reply(f"👋Hello {message.from_user.mention} \n\nWelcome Save Restricted Messages bot. This bot can help you to save restricted content from <b>public channel! Even upto 4GB file!</b>\n\n✍️Just Send me the link of the message🤩", parse_mode = enums.ParseMode.HTML, reply_markup = key)
 
 async def send(client, message):
 	now = datetime.datetime.now()
@@ -66,12 +100,15 @@ async def send(client, message):
 		if a.text:
 			await message.reply(a.text)
 			await message.reply("<b>🔥Hurray! I\'ve successfully saved your Text! Enjoy🤩</b>", parse_mode = enums.ParseMode.HTML)
+			return
+		if a.audio:
+			await app.send_video(message.chat.id, a.audio.file_id, caption = message.caption)
+			await message.reply("<b>🔥Hurray! I\'ve successfully saved your Text! Enjoy🤩</b>", parse_mode = enums.ParseMode.HTML)
 			return 
 		else:
 			await message.reply("⚠️Either i don\'t know this type of content! or I couldn't save it.")			
 	except Exception as e:
-		await message.reply("🔰Oppss! Make sure that the channel is public and the link is starts with <b>https://</b>", parse_mode = enums.ParseMode.HTML)
-		await message.reply(e)
+		await message.reply("🔰Oppss! Make sure that the channel is public and the link is starts with <b>https://</b>", parse_mode = enums.ParseMode.HTML)	
 
 @app.on_message()
 async def down(client, message):
@@ -81,7 +118,7 @@ async def down(client, message):
 	if user and 'bonus_time' in user:
 		bonus_time = user['bonus_time']
 		if now - bonus_time < datetime.timedelta(minutes=1):
-			await app.send_message(chat_id, '🙂You have to wait 1 minute in order to send another task! 😏Don\'t disturb me!😊')
+			await app.send_message(chat_id, '⌚You have to wait 1 minute in order to send another task! 😏Don\'t disturb me!😊')
 			return 			
 		else:
 			await send(client, message)
